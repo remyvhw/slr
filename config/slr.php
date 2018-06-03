e4762a82b093459c0ea82e65e4c60c9dc1eee5f6
Remy Vanherweghem <remy.vanherweghem@pumax.info>
2018-06-03 10:47:32 -0400

    Added app specific config file.

---
+++ config/slr.php
@@ -0,0 +1,18 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Obstructions Source
+    |--------------------------------------------------------------------------
+    |
+    | This value is the source of the obstructions
+    |
+     */
+
+    'obstructions' => [
+        "url" => "https://rem.info/fr/api/obstruction",
+    ],
+
+];
