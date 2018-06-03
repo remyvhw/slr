d2afb1deee7eef9df2c6b3472c54a1419b5b3086
Remy Vanherweghem <remy.vanherweghem@pumax.info>
2018-06-03 15:31:12 -0400

    Made obstruction model auditable.

--- app/Obstruction.php
+++ app/Obstruction.php
@@ -3,9 +3,12 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use OwenIt\Auditing\Contracts\Auditable;
 
-class Obstruction extends Model
+class Obstruction extends Model implements Auditable
 {
+    use \OwenIt\Auditing\Auditable;
+
     public $incrementing = false;
     public $keyType = "string";
 
