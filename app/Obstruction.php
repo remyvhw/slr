30dc4705f6eecfaacc571653c164d7119c063f9d
Remy Vanherweghem <remy.vanherweghem@pumax.info>
2018-06-03 18:48:24 -0400

    Added softdeletes trait to Obstruction model.

--- app/Obstruction.php
+++ app/Obstruction.php
@@ -3,13 +3,22 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 use OwenIt\Auditing\Contracts\Auditable;
 
 class Obstruction extends Model implements Auditable
 {
     use \OwenIt\Auditing\Auditable;
+    use SoftDeletes;
 
     public $incrementing = false;
     public $keyType = "string";
+
+    /**
+     * The attributes that should be mutated to dates.
+     *
+     * @var array
+     */
+    protected $dates = ['deleted_at'];
 
 }
