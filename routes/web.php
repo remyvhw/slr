2a9dcbc566a85b5825b9faffc45c4333833cdc64
Remy Vanherweghem <remy.vanherweghem@pumax.info>
2018-06-03 22:13:24 -0400

    Store last visit date in session.

--- routes/web.php
+++ routes/web.php
@@ -9,8 +9,9 @@
 | routes are loaded by the RouteServiceProvider within a group which
 | contains the "web" middleware group. Now create something great!
 |
-*/
+ */
 
 Route::get('/', function () {
+    session()->put('last_visit', now());
     return view('welcome');
 });
