0a2ec60dff100c3ffb7eb6f856dcf9036225e851
Remy Vanherweghem <remy.vanherweghem@pumax.info>
2018-06-06 20:18:11 -0400

    Renamed obstructions map component to be a little clearer.

--- resources/assets/js/components/ObstructionBrowser.vue
+++ resources/assets/js/components/ObstructionBrowser.vue
@@ -1,20 +1,20 @@
 <template>
-    <div class="flex flex-wrap">
-        <div class="w-full lg:w-2/3">
-            <div class="h-64 lg:h-screen">
-                <obstruction-map></obstruction-map>
-            </div>
-        </div>
-        <div class="w-full h-screen lg:overflow-y-scroll bg-grey-lighter lg:w-1/3 py-4">
-            <div class="w-full text-center">
-                <div class="self-center"><img src="/logo.svg" class="h-10 w-10"></div>
-            </div>
-            <div>
-                <obstruction-item v-for="obstruction in obstructions" :key="obstruction.id" :obstruction="obstruction"></obstruction-item>
-
-            </div>
-        </div>
+  <div class="flex flex-wrap">
+    <div class="w-full lg:w-2/3">
+      <div class="h-64 lg:h-screen">
+        <obstructions-map></obstructions-map>
+      </div>
     </div>
+    <div class="w-full h-screen lg:overflow-y-scroll bg-grey-lighter lg:w-1/3 py-4">
+      <div class="w-full text-center">
+        <div class="self-center"><img src="/logo.svg" class="h-10 w-10"></div>
+      </div>
+      <div>
+        <obstruction-item v-for="obstruction in obstructions" :key="obstruction.id" :obstruction="obstruction"></obstruction-item>
+
+      </div>
+    </div>
+  </div>
 </template>
 
 <script type="text/babel">
@@ -28,7 +28,7 @@ export default {
     }
   },
   components: {
-    obstructionMap: require("./ObstructionMap.vue"),
+    obstructionsMap: require("./ObstructionsMap.vue"),
     obstructionItem: require("./ObstructionItem.vue")
   },
   mounted() {
