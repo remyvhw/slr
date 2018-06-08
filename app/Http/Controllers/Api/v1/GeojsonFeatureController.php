f5125764e2a923dc54083e388a10e7b50da2a78a
Remy Vanherweghem <remy.vanherweghem@pumax.info>
2018-06-08 18:32:40 -0400

    Scaffolded an API controller for the GeojsonFeatures.

---
+++ app/Http/Controllers/Api/v1/GeojsonFeatureController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers\Api\v1;
+
+use App\GeojsonFeature;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class GeojsonFeatureController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\GeojsonFeature  $geojsonFeature
+     * @return \Illuminate\Http\Response
+     */
+    public function show(GeojsonFeature $geojsonFeature)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\GeojsonFeature  $geojsonFeature
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, GeojsonFeature $geojsonFeature)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\GeojsonFeature  $geojsonFeature
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(GeojsonFeature $geojsonFeature)
+    {
+        //
+    }
+}
