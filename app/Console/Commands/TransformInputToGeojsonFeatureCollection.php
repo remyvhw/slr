c5444665cd9d7aafaec0c3d35e106f019ef8f171
Remy Vanherweghem <remy.vanherweghem@pumax.info>
2018-06-08 18:10:59 -0400

    REM raw geojson converter shim now converts lines.

--- app/Console/Commands/TransformInputToGeojsonFeatureCollection.php
+++ app/Console/Commands/TransformInputToGeojsonFeatureCollection.php
@@ -2,6 +2,7 @@
 
 namespace App\Console\Commands;
 
+use App\GeojsonFeature;
 use Illuminate\Console\Command;
 
 class TransformInputToGeojsonFeatureCollection extends Command
@@ -9,16 +10,18 @@ class TransformInputToGeojsonFeatureCollection extends Command
     /**
      * The name and signature of the console command.
      *
+     *
+     *
      * @var string
      */
-    protected $signature = 'geojson:input';
+    protected $signature = 'geojson:input {input?}'; // `php artisan geojson:input < ~/myfile.json`
 
     /**
      * The console command description.
      *
      * @var string
      */
-    protected $description = 'Command description';
+    protected $description = 'Take a raw JSON string from REM\'s website, transform it to GeojsonFeature objects and save them.';
 
     /**
      * Create a new command instance.
@@ -30,6 +33,30 @@ class TransformInputToGeojsonFeatureCollection extends Command
         parent::__construct();
     }
 
+    protected function extractLines(array $lines)
+    {
+        collect($lines)->map(function ($line) {
+            return [
+                "type" => "Feature",
+                "properties" => [
+                    "stroke" => "#85bb23",
+                    "stroke-width" => "2",
+                    "stroke-opacity" => "1",
+                    "name" => array_get($line, "name"),
+                ],
+                "geometry" => [
+                    "type" => "LineString",
+                    "coordinates" => array_get($line, "coordinates"),
+                ],
+            ];
+        })->each(function ($feature) {
+            GeojsonFeature::create([
+                'name' => str_slug(str_limit(array_get($feature, "properties.name", ""))),
+                'payload' => json_encode($feature),
+            ]);
+        });
+    }
+
     /**
      * Execute the console command.
      *
@@ -37,6 +64,15 @@ class TransformInputToGeojsonFeatureCollection extends Command
      */
     public function handle()
     {
-        //
+        $data = json_decode(file_get_contents('php://stdin'), true);
+
+        if (!array_has($data, "rem_lines")) {
+            throw new \RuntimeException("Invalid input JSON. Copy raw data from REM source.");
+        }
+
+        GeojsonFeature::truncate();
+
+        $this->extractLines(array_get($data, "rem_lines"));
+
     }
 }
--- app/GeojsonFeature.php
+++ app/GeojsonFeature.php
@@ -13,4 +13,11 @@ class GeojsonFeature extends Model
      */
     public $timestamps = false;
 
+    /**
+     * The attributes that aren't mass assignable.
+     *
+     * @var array
+     */
+    protected $guarded = [];
+
 }
