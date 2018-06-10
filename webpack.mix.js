21ea507cb70f95b5f3b05627c6876ce559e39878
Remy Vanherweghem <remy.vanherweghem@pumax.info>
2018-06-09 20:51:25 -0400

    Updated webpack config so mapbox is loaded from the CDN (fixes issue with using uglify).

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,16 +1,11 @@
 let mix = require('laravel-mix');
 var tailwindcss = require('tailwindcss');
 
-/*
- |--------------------------------------------------------------------------
- | Mix Asset Management
- |--------------------------------------------------------------------------
- |
- | Mix provides a clean, fluent API for defining some Webpack build steps
- | for your Laravel application. By default, we are compiling the Sass
- | file for the application as well as bundling up all the JS files.
- |
- */
+mix.webpackConfig({
+    externals: {
+        'mapbox-gl': 'mapboxgl'
+    }
+});
 
 mix.js('resources/assets/js/app.js', 'public/js')
     .sass('resources/assets/sass/app.scss', 'public/css')
