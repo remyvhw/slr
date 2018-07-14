9aeb5b832254da2ba905ecda17a3e56851505761
Remy Vanherweghem <remy.vanherweghem@pumax.info>
2018-07-13 23:52:56 -0400

    Fixed map to top on mobile.

--- resources/assets/js/components/ObstructionsBrowser.vue
+++ resources/assets/js/components/ObstructionsBrowser.vue
@@ -1,16 +1,18 @@
 <template>
   <div class="flex flex-wrap">
-    <div class="w-full lg:w-2/3">
+    <div class="w-full fixed lg:relative lg:w-2/3">
       <div class="h-64 lg:h-screen">
         <obstructions-map :api-endpoint="featuresApiEndpoint"></obstructions-map>
       </div>
     </div>
-    <div class="w-full h-screen lg:overflow-y-scroll bg-grey-lighter lg:w-1/3 py-4">
-      <div class="w-full text-center">
-        <div class="self-center"><img src="/logo.svg" class="h-10 w-10"></div>
-      </div>
+    <div class="w-full h-screen lg:overflow-y-scroll bg-grey-lighter lg:w-1/3 mt-64 lg:mt-0">
+      <div class=" py-4">
+        <div class="w-full text-center">
+          <div class="self-center"><img src="/logo.svg" class="h-10 w-10"></div>
+        </div>
 
-      <obstructions-list :api-endpoint="obstructionsApiEndpoint"></obstructions-list>
+        <obstructions-list :api-endpoint="obstructionsApiEndpoint"></obstructions-list>
+      </div>
     </div>
   </div>
 </template>
