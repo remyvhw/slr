336eec4d8737ba4a6166a818322f9f1348d05a24
Remy Vanherweghem <remy.vanherweghem@pumax.info>
2018-07-14 11:23:45 -0400

    Fixed sidebar background size not scaling all the way down on mobile.

--- resources/assets/js/components/ObstructionsBrowser.vue
+++ resources/assets/js/components/ObstructionsBrowser.vue
@@ -5,7 +5,7 @@
         <obstructions-map :api-endpoint="featuresApiEndpoint"></obstructions-map>
       </div>
     </div>
-    <div class="w-full h-screen lg:overflow-y-scroll bg-grey-lighter lg:w-1/3 mt-64 lg:mt-0">
+    <div class="w-full min-h-screen lg:overflow-y-scroll bg-grey-lighter lg:w-1/3 mt-64 lg:mt-0">
       <div class=" py-4">
         <div class="w-full text-center">
           <div class="self-center"><img src="/logo.svg" class="h-10 w-10"></div>
