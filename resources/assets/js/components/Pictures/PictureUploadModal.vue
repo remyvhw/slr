09b49703bce87433ba10220719b9ddea10887484
Remy Vanherweghem <remy.vanherweghem@pumax.info>
2018-07-16 14:21:14 -0400

    Scaffolded a modal picture uploader.

---
+++ resources/assets/js/components/Pictures/PictureUploadModal.vue
@@ -0,0 +1,22 @@
+
+<template>
+    <generic-modal @close="closeModal" title="Envoi de photos">
+
+    </generic-modal>
+
+</template>
+
+<script type="text/babel">
+const marked = require("marked");
+
+export default {
+  components: {
+    genericModal: require("../GenericModal.vue")
+  },
+  methods: {
+    closeModal() {
+      this.$emit("close");
+    }
+  }
+};
+</script>
