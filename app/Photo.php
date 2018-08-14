46b4187c49993e13995f58486bfdcc03605ec07f
Remy Vanherweghem <remy.vanherweghem@pumax.info>
2018-08-14 14:43:19 -0400

    Dispatches photo created event from Photo model.

--- app/Photo.php
+++ app/Photo.php
@@ -2,10 +2,21 @@
 
 namespace App;
 
+use App\Events\PhotoCreated;
 use Illuminate\Database\Eloquent\Model;
 
 class Photo extends Model
 {
+
+    /**
+     * The event map for the model.
+     *
+     * @var array
+     */
+    protected $dispatchesEvents = [
+        'created' => PhotoCreated::class,
+    ];
+
     public function getStoragePathAttribute()
     {
         return $this->id ? "photos/orig/$this->id" : null;
