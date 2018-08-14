5cf6b7d52dc040d8cee6d6580f0c36dbea2411dd
Remy Vanherweghem <remy.vanherweghem@pumax.info>
2018-08-14 11:39:31 -0400

    Built a basic Station object.

---
+++ database/migrations/2018_08_14_153331_create_stations_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateStationsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('stations', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string("lat");
+            $table->string("lng");
+            $table->string("name", 255);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('stations');
+    }
+}
