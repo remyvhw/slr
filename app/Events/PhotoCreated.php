202fbb9ee4db5eb7616ba91f57bf8e8ec586ad9a
Remy Vanherweghem <remy.vanherweghem@pumax.info>
2018-08-14 14:23:03 -0400

    Renamed photo saved event to created.

--- app/Events/PhotoCreated.php
+++ app/Events/PhotoCreated.php
@@ -10,7 +10,7 @@ use Illuminate\Foundation\Events\Dispatchable;
 use Illuminate\Broadcasting\InteractsWithSockets;
 use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
 
-class PhotoSaved
+class PhotoCreated
 {
     use Dispatchable, InteractsWithSockets, SerializesModels;
 
--- app/Providers/EventServiceProvider.php
+++ app/Providers/EventServiceProvider.php
@@ -13,7 +13,7 @@ class EventServiceProvider extends ServiceProvider
      * @var array
      */
     protected $listen = [
-        'App\Events\PhotoSaved' => [
+        'App\Events\PhotoCreated' => [
             'App\Listeners\MatchPhotoWithStation',
         ],
     ];
